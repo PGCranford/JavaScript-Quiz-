e480675bcaee0f671945fe8f043ee19ae5c0b3b7
PGCranford <PGCranford@gmail.com>
2022-05-28 11:17:17 -0400

    button variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,16 @@ var buttonBEL = document.getElementById("answerB")
 var buttonCEL = document.getElementById("answerC")
 var buttonDEL = document.getElementById("answerD")
 
-var questions = ["Inside which HTML element do we put the JavaScript?", "What is not a Primitive JavaScript?", "JavaScript is a _____ language?",
+var questions = ["Inside which HTML element do we put the JavaScript?", "What is not a  JavaScript Primitive Data Type?", "JavaScript is a _____ language?",
   "Which of the following methods can be used to display data?", "How can a data type be declared to be a constant type?"]
 
 var answerOne = ["<javascript>", "<scripting>", "<js>", "<script>"]
+var answerTwo = ["String", "Boolean", "Array", "Number"]
+var answerThree = ["Object-Based", "Object-Oriented", "Procedural", "Foreign"]
+var answerFour = ["document.write()", "console.log()", "window.alert()", "All of the above"]
+var answerFive = ["var", "const", "constant", "let"]
+
+
 
 document.getElementById("startBtn").addEventListener("click", function () {
   startTime;
@@ -26,40 +32,32 @@ document.getElementById("startBtn").addEventListener("click", function () {
 
   }
   valId = setInterval(startTime, 1000);
-  round1();
+
 });
-
-
 //if (time >= 0) {
 
-var round1 = function () {
-  document.getElementById("questions").innerHTML = questions[0]
-  buttonAEL.textContent = answerOne[0]
-  buttonBEL.textContent = answerOne[1]
-  buttonCEL.textContent = answerOne[2]
-  buttonDEL.textContent = answerOne[3]
+var round1 =
+  document.getElementById("questions").textContent = questions[0]
+buttonAEL.textContent = answerOne[0]
+buttonBEL.textContent = answerOne[1]
+buttonCEL.textContent = answerOne[2]
+buttonDEL.textContent = answerOne[3]
 
-  //correct answer
-  addEventListener("click", buttonAEL), function () {
-    score = score + 1;
-    console.log(score);
-
-  };
-  // incorrect answer
-  // else (
-  //   time = time - 2
-  // );
+//correct answer
+// if (addEventListener("click", buttonAEL)) {
+//   score = score + 1,
+//     console.log(score),
+// }
 
 
-};
+// //incorrect answer
+// else (addEventListener("click", buttonBEL, buttonCEL, buttonDEL)){
+//   time = time - 2;
+// }
 
-
-var round2 = function () {
-  document.getElementById("questions").innerHTML = question2;
-  document.getElementById("answer-A").innerHTML = "javascript";
-  document.getElementById("answer-B").innerHTML = "scripting";
-  document.getElementById("answer-C").innerHTML = "js";
-  document.getElementById("answer-D").innerHTML = "script";
-
-
-}
+var round2 =
+  document.getElementById("questions").innerHTML = questions[1];
+buttonAEL.textContent = answerTwo[0]
+buttonAEL.textContent = answerTwo[1]
+buttonAEL.textContent = answerTwo[2]
+buttonAEL.textContent = answerTwo[3]
